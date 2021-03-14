b68b87fbde92b0522e5dc749e37e533fd983e485
ten986 <tenkichi986@yahoo.co.jp>
2021-03-15 00:34:32 +0900

    add comment

--- components/CheckboxList.tsx
+++ components/CheckboxList.tsx
@@ -1,20 +1,24 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 
+// APIの返り値の型
 type PrefectureResponse = {
   message: string
   result: Prefecture[]
 }
 
+// APIの返り値の型
 type Prefecture = {
   prefCode: number
   prefName: string
 }
 
 const CheckboxList: React.FC = () => {
+  // 都道府県一覧
   const [prefectures, setPrefectures] = useState<Prefecture[]>([])
 
   useEffect(() => {
+    // 都道府県一覧を取得する
     const getPrefectures = async () => {
       const result = await axios
         .get<PrefectureResponse>(
