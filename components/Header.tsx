465b1948b78f234a8dfb766fe88c4d9c1f1a4e92
ten986 <tenkichi986@yahoo.co.jp>
2021-03-16 01:49:06 +0900

    ヘッダーのcssを定義、そのためのグローバルcssの定義
    ヘッダーのcssを作った
    body{margin: 0} にするために、グローバルcssを定義した
    ところで、app.tsxがなかったので作った

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import styles from './Header.module.css'
+
 // チェックボックス
 const Header: React.FC = () => {
-  return <header>各都道府県の人口を表示するアプリ</header>
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>各都道府県の人口を表示するアプリ</div>
+    </header>
+  )
 }
 
 export default Header
