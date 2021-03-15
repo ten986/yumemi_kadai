a945183564dd1bcb73ddfe90617fc35d9ec5d022
ten986 <tenkichi986@yahoo.co.jp>
2021-03-15 18:00:45 +0900

    都道府県取得を外に出した

--- components/CheckboxList.tsx
+++ components/CheckboxList.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  ChangeEventHandler,
-  useMemo,
-  useCallback,
-} from 'react'
+import React, { ChangeEventHandler, useMemo, useCallback } from 'react'
 import { Prefecture } from '../pages/api/prefectures'
 import CheckBox from './Checkbox'
 
@@ -12,25 +6,16 @@ type CheckedItemMap = Map<string, boolean>
 
 type Props = {
   // チェックボックスの値を保持する
+  prefectures: Prefecture[]
   checkedItems: CheckedItemMap
   setCheckedItems: React.Dispatch<React.SetStateAction<CheckedItemMap>>
 }
 
-const CheckboxList: React.FC<Props> = ({ checkedItems, setCheckedItems }) => {
-  // 都道府県一覧
-  const [prefectures, setPrefectures] = useState<Prefecture[]>([])
-
-  useEffect(() => {
-    // 都道府県一覧を取得する
-    const getPrefectures = async () => {
-      const response = await fetch('/api/prefectures')
-      const result: Prefecture[] = await response.json()
-      setPrefectures(() => result)
-    }
-
-    getPrefectures()
-  }, [])
-
+const CheckboxList: React.FC<Props> = ({
+  prefectures,
+  checkedItems,
+  setCheckedItems,
+}) => {
   // チェックボックスが押された時の挙動
   const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
     (e) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CheckboxList from '../components/CheckboxList'
 import PopulationGraph from '../components/PopulationGraph'
+import { Prefecture } from './api/prefectures'
 
 const IndexPage: React.FC = () => {
+  // 都道府県一覧
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([])
+
+  useEffect(() => {
+    // 都道府県一覧を取得する
+    const getPrefectures = async () => {
+      const response = await fetch('/api/prefectures')
+      const result: Prefecture[] = await response.json()
+      setPrefectures(() => result)
+    }
+
+    getPrefectures()
+  }, [])
+
   // チェックボックスの状態を保持する
   const [checkedItems, setCheckedItems] = useState<Map<string, boolean>>(
     new Map<string, boolean>(),
@@ -11,6 +26,7 @@ const IndexPage: React.FC = () => {
   return (
     <>
       <CheckboxList
+        prefectures={prefectures}
         checkedItems={checkedItems}
         setCheckedItems={setCheckedItems}
       />
