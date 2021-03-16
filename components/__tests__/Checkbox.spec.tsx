8760ec1311e42864b4469eb2ba059394bac5130d
ten986 <tenkichi986@yahoo.co.jp>
2021-03-16 16:55:14 +0900

    チェックボックスのテストを更新

--- components/__tests__/Checkbox.spec.tsx
+++ components/__tests__/Checkbox.spec.tsx
@@ -4,21 +4,44 @@ import { shallow } from 'enzyme'
 import Checkbox from '../Checkbox'
 
 describe('Checkbox', () => {
-  it('北海道でrenderする', () => {
-    const checked = false
-    const handleChange = () => {
-      checked
+  it('スナップショットが一致するか', () => {
+    const mockFn = jest.fn()
+    const props = {
+      id: 'checkbox',
+      value: '北海道',
+      checked: false,
+      onChange: mockFn,
     }
-    const checkbox = shallow(
-      <Checkbox
-        id="checkbox"
-        value="北海道"
-        checked={checked}
-        onChange={handleChange}
-      />,
-    )
+    const checkbox = shallow(<Checkbox {...props} />)
 
+    // スナップショット
     expect(checkbox).toMatchSnapshot()
+  })
+  it('onChangeが呼ばれるか', () => {
+    const mockFn = jest.fn()
+    const props = {
+      id: 'checkbox',
+      value: '北海道',
+      checked: false,
+      onChange: mockFn,
+    }
+    const checkbox = shallow(<Checkbox {...props} />)
+
+    // onChange が呼ばれるか
+    checkbox.find('div').find('input').simulate('change')
+    expect(mockFn.mock.calls.length).toEqual(1)
+  })
+  it('テキストがvalueと一致するか', () => {
+    const mockFn = jest.fn()
+    const props = {
+      id: 'checkbox',
+      value: '北海道',
+      checked: false,
+      onChange: mockFn,
+    }
+    const checkbox = shallow(<Checkbox {...props} />)
+
+    // テキストが北海道になるか
     expect(checkbox.text()).toBe('北海道')
   })
 })
